9ba5fe94952c3b5d2b3a72aa3c082c30c59aaab4
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-11-28 10:13:27 +0700

    new Task

---
+++ JS-Jquery/Kuis02/kuis02.js
@@ -0,0 +1,20 @@
+function tambah(p1, p2) {
+  return p1 + p2;
+}
+
+function kurang(p1, p2) {
+  return p1 - p2;
+}
+
+function kali(p1, p2) {
+  return p1 * p2;
+}
+
+function bagi(p1, p2) {
+  return p1 / p2;
+}
+
+console.log(tambah(6, 3));
+console.log(kurang(6, 3));
+console.log(kali(6, 3));
+console.log(bagi(6, 3));
