b886a0341dc21a602aaf38664faf0060b69d8d9a
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-12-23 15:08:59 +0700

    add styling

--- JS-Jquery/Ferdian_TA_JS/assets/js/script.js
+++ JS-Jquery/Ferdian_TA_JS/assets/js/script.js
@@ -1,12 +1,43 @@
 // c50d9de8240f4bb0a7724622222312
 
 $(document).ready(function () {
-  //   const getCard = (data) => {
-  //     return `
-  //     <div class="weather-card">
-  //     <div class="weather-card">
-  // `;
-  //   };
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const getCard = (data) => {
+    return `
+      <div class="col">
+        <div class="weather-card card-bg-${randomIntFromInterval(1, 3)}">
+          <p class="weather-value" id="weather-value">${
+            data.current.condition.text
+          }</p>
+          <div class="weather-detail-group row justify-content-start align-items-center">
+            <div class="temp-group col-md-3">
+              <h1 class="temp-value" id="temp-value">${Math.round(
+                data.current.temp_c
+              )}
+                <span class="temp-unit" id="temp-unit">°C</span>
+              </h1>
+            </div>
+            <div class="weather-detail col-md-6">
+              <p class="date-value m-0 p-0 mb-2" id="date-value">${
+                data.location.localtime
+              }</p>
+              <p class="location-value m-0 p-0" id="location-value">${
+                data.location.name
+              }, ${data.location.country}</p>
+            </div>
+            <div class="col-md-3">
+              <img src="${
+                data.current.condition.icon
+              }" alt="" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </div>
+  `;
+  };
 
   const getData = (name) => {
     $.ajax({
@@ -21,7 +52,7 @@ $(document).ready(function () {
       success: (res) => {
         console.log(res);
         swal.close();
-        // $("#weather-show.weather-show").append();
+        $("#weather-show.weather-show").append(getCard(res));
       },
 
       error: (err) => {
